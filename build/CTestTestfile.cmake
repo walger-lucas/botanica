1fb8d704f11d0bbd6ccced5b6bbf025b1774f1dc
Lucas Walger do Nascimento <walger.profissional@gmail.com>
2023-04-05 17:58:44 -0300

    Alteracoes cmake

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/walger/Desenvolvimento/c++/botanica/source
-# Build directory: /home/walger/Desenvolvimento/c++/botanica/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
