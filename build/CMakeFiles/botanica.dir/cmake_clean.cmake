d36480221dfd93ab50d2dddeb57bfd3be5555af9
Lucas Walger do Nascimento <walger.profissional@gmail.com>
2023-04-05 15:54:19 -0300

    Dados iniciais enviados para o repositorio, com default de wxWidgets

---
+++ build/CMakeFiles/botanica.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/botanica.dir/main.cpp.o"
+  "botanica"
+  "botanica.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/botanica.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
