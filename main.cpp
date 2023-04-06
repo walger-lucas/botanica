b2d1d50908757362c1073dd9b1dccaf57bbc0dfb
Lucas Walger do Nascimento <walger.profissional@gmail.com>
2023-04-05 23:00:56 -0300

    Alteraçoes na main com menus significativos

--- main.cpp
+++ main.cpp
@@ -19,15 +19,10 @@ public:
     MyFrame();
  
 private:
-    void OnHello(wxCommandEvent& event);
     void OnExit(wxCommandEvent& event);
-    void OnAbout(wxCommandEvent& event);
+
 };
  
-enum
-{
-    ID_Hello = 1
-};
  
 wxIMPLEMENT_APP(MyApp);
  
@@ -41,17 +36,20 @@ bool MyApp::OnInit()
 MyFrame::MyFrame()
     : wxFrame(nullptr, wxID_ANY, "Hello World")
 {
-    wxMenu *menuFile = new wxMenu;
-    menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
-                     "Help string shown in status bar for this menu item");
-    menuFile->AppendSeparator();
-    menuFile->Append(wxID_EXIT);
+    wxMenu *menuPlantas = new wxMenu;
+    menuPlantas->Append(wxID_ANY, "Nova &Planta...\tCtrl-N",
+                     "Crie uma nova planta.");
+    menuPlantas->AppendSeparator();
+    menuPlantas->Append(wxID_ANY, L"Nova &Espécie...\tCtrl-M",
+                     L"Adicione uma nova espécie de plantas.");
+    menuPlantas->AppendSeparator();
+    menuPlantas->Append(wxID_EXIT);
  
     wxMenu *menuHelp = new wxMenu;
     menuHelp->Append(wxID_ABOUT);
  
     wxMenuBar *menuBar = new wxMenuBar;
-    menuBar->Append(menuFile, "&File");
+    menuBar->Append(menuPlantas, "&Plantas");
     menuBar->Append(menuHelp, "&Help");
  
     SetMenuBar( menuBar );
@@ -59,23 +57,10 @@ MyFrame::MyFrame()
     CreateStatusBar();
     SetStatusText("Welcome to wxWidgets!");
  
-    Bind(wxEVT_MENU, &MyFrame::OnHello, this, ID_Hello);
-    Bind(wxEVT_MENU, &MyFrame::OnAbout, this, wxID_ABOUT);
     Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
 }
  
 void MyFrame::OnExit(wxCommandEvent& event)
 {
     Close(true);
-}
- 
-void MyFrame::OnAbout(wxCommandEvent& event)
-{
-    wxMessageBox("This is a wxWidgets Hello World example",
-                 "About Hello Walger", wxOK | wxICON_INFORMATION);
-}
- 
-void MyFrame::OnHello(wxCommandEvent& event)
-{
-    wxLogMessage("Hello wold from wxWidgets!");
 }
